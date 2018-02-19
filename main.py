ada9ed26e3b1fb67734929ef66bf4a29c705c5ce
munhyunsu <munhyunsu@gmail.com>
2018-02-19 07:10:37 -0500

    db name

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config.from_object(__name__) # load config from this file, flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE = os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE = os.path.join(app.root_path, 'tashu.db'),
     SECRET_KEY = 'developemnt key',
     USERNAME = 'admin',
     PASSWORD = 'default'
